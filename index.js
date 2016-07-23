017e47d42b1647a5841c471dc4ad7277d74b1d4c
Piet Geursen <pietgeursen@gmail.com>
2016-07-23 22:43:17 +1200

    Borders work okish

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ const app = {
       var newModel = Immutable(model).set('timer', action.payload) 
       return {model: newModel.asMutable({deep: true})}
       break;
-      
+      case 'TOGGLE_NOTE':
+      var newModel = Immutable(model).setIn(['notes', action.payload], !model.notes[action.payload] ) 
+      return {model: newModel.asMutable({deep: true})}
+
     }
     return {model}
   },
   view: function (model, dispatch) {
     return html`<main>
       ${model.notes.map(function(note, index) {
-       return html`<div class=${classnames({playing: model.index === index, on: note})}>note</div>` 
+       return html`<div onclick=${()=> dispatch({type: 'TOGGLE_NOTE', payload: index})} class=${classnames({playing: model.index === index, on: note}, 'note')}></div>` 
       })}
     </main>`
   },
