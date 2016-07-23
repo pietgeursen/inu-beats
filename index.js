e78381503ac7b97d229bb1f3e80d09fe68f8e03f
Piet Geursen <pietgeursen@gmail.com>
2016-07-23 20:32:29 +1200

    Cycling classes.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-import {start, pull, html} from 'inu'
+import { start, pull, html } from 'inu'
+import Pushable from 'pull-pushable'
+import Immutable from 'seamless-immutable'
 
 const app = {
-
-  init: function() {
-   return {model: {greeting: 'hello, you are awesome!'}} 
+  init: function () {
+    return Immutable({
+      model: {
+        notes: [true, false, false, false],
+        index: 0
+      },
+      effect: {type: 'SCHEDULE_TICK', payload: 300}
+    })
   },
-  update: function(model, action) {
-   return {model} 
+  update: function (model, action) {
+    //console.log(model, action);
+    switch(action.type){
+      case 'TICK':
+      const newModel = model.set('index', (model.index + 1) % model.notes.length) 
+      return {model: newModel}
+    }
+    return {model}
   },
-  view: function(model, dispatch) {
-    return html`<main>${model.greeting}</main>` 
+  view: function (model, dispatch) {
+    const notes = model.notes.asMutable()
+    return html`<main>
+      ${notes.map(function(note, index) {
+       return html`<div class=${model.index === index ? 'playing' : ''}>note</div>` 
+      })}
+    </main>`
   },
-  run: function(effect, streams) {
-    
+  run: function (effect, sources) {
+    console.log(effect);
+    switch(effect.type){
+      case 'SCHEDULE_TICK': 
+        const p = Pushable()
+        setInterval(() => p.push({type: 'TICK'}), effect.payload)
+        return p
+    } 
   }
 }
 
@@ -20,6 +44,6 @@ const {views} = start(app)
 
 const main = document.querySelector('main')
 
-pull(views(), pull.drain(function(view) {
-  html.update(main, view)  
+pull(views(), pull.drain(function (view) {
+  html.update(main, view)
 }))
